611f44fb45d6ae6438a86a2fbe4c6014d310bdc1
johngordon70 <johngordon70@gmail.com>
2016-12-01 19:47:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,29 +5,29 @@ var botID = process.env.BOT_ID;
 
 function respond() {
     var request = JSON.parse(this.req.chunks[0]),
-        botRegex = /^\/cool guy$/;
+    botRegex = /^\/cool guy$/;
     botRegex = /^\/vape$/;
     botRegex = /^\/Pass the butter$/;
     botRegex = /^\/You pass butter$/;
     botRegex = /^\/Open the bay doors Hal$/;
-    if(request.text && botRegex.test(request.text)) {
+    if (request.text && botRegex.test(request.text)) {
         this.res.writeHead(200);
         postMessage();
         this.res.end();
     }
-        
-    else if (request.text && botregex.Passthebutter(request.text)) {
+
+    else if (request.text && botRegex.Passthebutter(request.text)) {
         this.res.writeHead(200);
         postMessage("*passes butter* \nWhat is my purpose?");
         this.res.end();
     }
 
-    else if (request.text && botregex.Youpassbutter(request.text)) {
+    else if (request.text && botRegex.Youpassbutter(request.text)) {
         this.res.writeHead(200);
         postMessage("...\noh my god");
         this.res.end();
 
-    }   else {
+    } else {
         console.log("don't care");
         this.res.writeHead(200);
         this.res.end();
@@ -46,25 +46,25 @@ function postMessage() {
     };
 
     body = {
-        "bot_id" : botID,
-        "text" : botResponse
+        "bot_id": botID,
+        "text": botResponse
     };
 
     console.log('sending ' + botResponse + ' to ' + botID);
 
-    botReq = HTTPS.request(options, function(res) {
-        if(res.statusCode == 202) {
+    botReq = HTTPS.request(options, function (res) {
+        if (res.statusCode == 202) {
             //neat
         } else {
             console.log('rejecting bad status code ' + res.statusCode);
         }
     });
 
-    botReq.on('error', function(err) {
-        console.log('error posting message '  + JSON.stringify(err));
+    botReq.on('error', function (err) {
+        console.log('error posting message ' + JSON.stringify(err));
     });
-    botReq.on('timeout', function(err) {
-        console.log('timeout posting message '  + JSON.stringify(err));
+    botReq.on('timeout', function (err) {
+        console.log('timeout posting message ' + JSON.stringify(err));
     });
     botReq.end(JSON.stringify(body));
 }
