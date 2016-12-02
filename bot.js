8eeb66784011b9bd781b7f8eccfa6a8c5b2fcb53
johngordon70 <johngordon70@gmail.com>
2016-12-01 20:23:50 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,18 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
+      botRegts = /^\/test$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
+    
+   if(request.text && botRegts.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Test Successful");
+    this.res.end();
+    
   } else {
     console.log("don't care");
     this.res.writeHead(200);
